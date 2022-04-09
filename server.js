6fe70086ef8738ee14b09d8b5bfda501736b3b7d
Bayorwor <bayorwor60@gmail.com>
2022-04-09 17:12:00 +0000

    basic middlewares

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const blogs = require("./data/blogs");
+const morgan = require("morgan");
 
 const app = express();
 
 app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"));
+app.use(morgan("dev"));
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
